73073f2be88d65e32ba9aa9c992b4d3652242935
Anantgs <anantgsaraf123@gmail.com>
2024-08-11 04:00:54 +0530

    ALB module added

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c7-03-ec2instance-bastion.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c7-03-ec2instance-bastion.tf
@@ -1,4 +1,5 @@
 module "ec2_public" {
+  depends_on = [ module.vpc ] 
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.1"
   name = "${var.environment}-BastionHost"
@@ -6,7 +7,8 @@ module "ec2_public" {
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   monitoring             = true
-  vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+  vpc_security_group_ids = [module.public_bastion_sg.security_group_id,module.private_sg.security_group_id]
+  user_data              = file("${path.module}/app1-install.sh")  
   subnet_id              = module.vpc.public_subnets[0]
   ami                    = data.aws_ami.amzlinux2.id
   tags                   = local.common_tags
