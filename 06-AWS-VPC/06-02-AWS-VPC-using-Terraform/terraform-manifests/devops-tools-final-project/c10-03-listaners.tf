3b08b508fe61c241aa94c15669cf2df226d85a2d
Anantgs <anantgsaraf123@gmail.com>
2024-08-20 18:26:07 +0530

    Removed comments

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/devops-tools-final-project/c10-03-listaners.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/devops-tools-final-project/c10-03-listaners.tf
@@ -11,18 +11,6 @@ resource "aws_lb_listener" "app_listener" {
   }
 }
 
-# # Register EC2 Instances with the Target Group
-# resource "aws_lb_target_group_attachment" "app_tg_attachment" {
-#   for_each = {
-#     for inst_key, inst_value in module.ec2_public : inst_key => inst_value
-#     if inst_value.tags_all["purpose"] == aws_lb_target_group.test[inst_value.tags_all["purpose"]].name
-#   }
-
-#   target_group_arn = aws_lb_target_group.test[each.value.tags_all["purpose"]].arn
-#   target_id        = each.value
-#   port             = 80
-# }
-
 locals {
   # Define a set of allowed purposes based on the variable instance_names
   allowed_purposes = toset(values(var.instance_names))
@@ -75,8 +63,6 @@ output "for_each_map" {
   description = "Map used in the for_each loop for target group attachment"
   value       = local.for_each_map
 }
-
-
 
 resource "aws_lb_target_group_attachment" "app_tg_attachment" {
   for_each = local.for_each_map
